48acde7cc6ced411a915ac8dc05e906a83d48b4e
Kristen Scheffler <kris10ben10@gmail.com>
2023-10-26 22:01:35 -0400

    auto clicker button

--- app.js
+++ app.js
@@ -12,19 +12,61 @@ setInterval(function (){
 }, 1000)
 
 const donutCountDisplay = document.getElementById("donutCount");
+const autoClickerCountDisplay = document.getElementById("autoClickerCountButton");
+const autoClickerCostDisplay = document.getElementById("autoClickerCostButton");
 const clickButton = document.getElementById("clickButton");
+const buyAutoClickerButton = document.getElementById("buyAutoClickerButton");
+
 let donutCount = 0;
+let autoClickerCount = 0;
+let autoClickerCost = 100;
+const costIncreasePercentage = 10;
 
 
 function updateDonutCount() {
     donutCountDisplay.textContent = donutCount;
 }
 
+function updateAutoClickerCount () {
+    autoClickerCostDisplay.textContent = autoClickerCost;
+}
+
+function updateAutoClickerCost() {
+    autoClickerCostDisplay.textContent = autoClickerCost;
+}
+
+function updateAutoClickerButtonState() {
+    if (donutCount >= autoClickerCost) {
+        buyAutoClickerButton.disabled = false;
+        buyAutoClickerButton.textContent = `Buy Auto-Clicker (${autoClickerCost} Donuts)`;
+    } else {
+        buyAutoClickerButton.disabled = true;
+        buyAutoClickerButton.textContent = `Buy Auto-Clicker (${autoClickerCost} Donuts)`;
+    }
+}
+
+function purchaseAutoClicker() {
+    if (donutCount >= autoClickerCost) {
+        donutCount -= autoClickerCost;
+        autoClickerCount++;
+        autoClickerCost = Math.round(autoClickerCost * (1 + costIncreasePercentage / 100)); // Increase cost
+        updateDonutCount();
+        updateAutoClickerCount();
+        updateAutoClickerCost();
+        updateAutoClickerButtonState();
+    }
+}
+
 clickButton.addEventListener("click", () => {
     donutCount++;
     updateDonutCount();
+    updateAutoClickerButtonState();
 });
 
+buyAutoClickerButton.addEventListener("click", purchaseAutoClicker);
+
+updateAutoClickerButtonState();
+updateAutoClickerCost();
 updateDonutCount();
 
 
